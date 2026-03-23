97c1bd3636f14cb0a07edb67ddfeb90ce158d660
Gustavo Pasquini <gustavopasquini2006@hotmail.com>
2026-03-22 23:08:37 -0300

    Ajustando path de projetos

--- src/main/java/unicesumar/gerenciamento_de_projetos/Controllers/ProjetoController.java
+++ src/main/java/unicesumar/gerenciamento_de_projetos/Controllers/ProjetoController.java
@@ -11,7 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "projetos")
+@RequestMapping(path = "/projetos")
 public class ProjetoController {
 
     @Autowired
